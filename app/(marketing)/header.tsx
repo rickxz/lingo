98419309c2ca5a304b5c1f4b93f10252c4a929e4
rickxz <herick.victor.rodrigues@hotmail.com>
2024-05-21 17:09:05 -0300

    feat: setup auth with clerk

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,32 @@
+import { Button } from '@/components/ui/button'
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { Loader } from 'lucide-react'
+import Image from "next/image";
+
 export function Header() {
  return (
   <header className="h-20 w-ful border-b-2 border-slate-200 px-4">
-    Header
+    <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+       <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+        <Image src="/mascot.svg" alt="Mascot" width={40} height={40} />
+        <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">Lingo</h1>
+       </div>
+       <ClerkLoading>
+        <Loader className="size-5 text-muted-foreground animate-spin" />
+       </ClerkLoading>
+       <ClerkLoaded>
+        <SignedIn>
+          <UserButton afterSignOutUrl='/' />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button size="lg" variant="ghost">
+              Login
+            </Button>
+          </SignInButton>
+        </SignedOut>
+       </ClerkLoaded>
+    </div>
   </header>
  ) 
 }
