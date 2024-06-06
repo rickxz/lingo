2b1124889c3e47cf8f8e41e1e6c5a703de17437d
rickxz <herick.victor.rodrigues@hotmail.com>
2024-06-06 15:25:21 -0300

    chore: fix dependency issues

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher([])
+const isPublicRoute = createRouteMatcher(['/']);
+
 
 export default clerkMiddleware((auth, request) => {
-  if (isPublicRoute(request)) {
+  if (!isPublicRoute(request)) {
     auth().protect()
   }
 })
